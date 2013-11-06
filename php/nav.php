7ee9851a53b5c3a8a19020ac45da4186b9c90954
Eric 'Zander' Nelson <ci.eric.nelson@gmail.com>
2013-11-06 15:31:13 -0800

    php start

---
+++ php/nav.php
@@ -0,0 +1,14 @@
+<div class="nav clearfix">
+    <div class="button">
+        <a href="#">Test1</a>
+    </div>
+    <div class="button">
+        <a href="#">Test2</a>
+    </div>
+    <div class="button">
+        <a href="#">Test3</a>
+    </div>
+    <div class="button">
+        <a href="#">Test4</a>
+    </div>
+</div>
