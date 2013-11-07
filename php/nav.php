f03298e78c27dee51c9fe07ff416989e61451582
Eric 'Zander' Nelson <ci.eric.nelson@gmail.com>
2013-11-07 10:49:00 -0800

    php link fix

--- php/nav.php
+++ php/nav.php
@@ -8,10 +8,10 @@
 
 <div class="nav clearfix">
     <div class="button">
-        <a href="../php/index.php">Home</a>
+        <a href="/personal-site/php/index.php">Home</a>
     </div>
     <div class="button">
-        <a href="../php/characters/index.php">Characters</a>
+        <a href="/personal-site/php/characters/index.php">Characters</a>
     </div>
     <div class="button">
         <a href="#">Test3</a>
