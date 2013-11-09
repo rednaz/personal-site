189deb117d85257dc5a8af174aae1a3b882429d2
rednaz <zander.nelson@gmail.com>
2013-11-09 00:42:38 -0800

    added template

---
+++ php/allies/allies-template.php
@@ -0,0 +1,94 @@
+<!--
+
+    Template
+
+    created by: Eric 'Zander' Nelson
+    created on: November 6, 2013 
+    version ##: beta.1
+    
+    zandernelsoninc.com
+
+                                        -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-cell-Type" content-cell="text/html; charset=utf-8" />
+<link type="text/css" rel="stylesheet" href="../../css/sass.css" />
+<title>Template</title>
+</head>
+
+<body>
+
+<div class="container clearfix">
+
+	<?php include "../nav.php"; ?>
+
+	<div class="main clearfix">
+
+        <div class="panel">
+            <div class="content">
+                <h1>Allies</h1>
+
+                <h2>Template</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, ipsum sed viverra auctor, dolor est varius magna, quis sodales lectus felis eu arcu. Nulla non luctus libero, venenatis vestibulum purus. Nunc erat augue, sagittis vitae odio eget, feugiat commodo mi. Donec nisl lacus, tincidunt in erat ac, luctus ultricies nibh. Mauris odio sapien, porta ac molestie vel, mattis eget leo. Phasellus ultricies egestas tellus, at posuere neque fermentum non. Cras in molestie neque. Donec ac vehicula mi, non commodo velit. Nam blandit eleifend lectus a vestibulum. Phasellus ut tortor et dui adipiscing vehicula. Maecenas lectus elit, sollicitudin quis ligula non, tempus consequat erat. Quisque laoreet urna eu nisi scelerisque, eget pulvinar mi fringilla. Donec nec dignissim augue.</p>
+            
+                </div>
+        </div>
+
+        <div class="panel1">
+            <div class="content-cell">
+                <table class="char-table">
+                    <tbody>
+                        <tr>
+                            <td>Player</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Race</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Class</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Level</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Alignment</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Affiliations</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="panel1">
+            <div class="content-cell">
+                <h3>Catch Phrases</h3>
+                <blockquote class="char-quotes">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </blockquote>
+            </div>
+        </div>
+        <div class="panel1">
+            <div class="content-cell">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, ipsum sed viverra auctor, dolor est varius magna, quis sodales lectus felis eu arcu. Nulla non luctus libero, venenatis vestibulum purus. Nunc erat augue, sagittis vitae odio eget, feugiat commodo mi. Donec nisl lacus, tincidunt in erat ac, luctus ultricies nibh. Mauris odio sapien, porta ac molestie vel, mattis eget leo. Phasellus ultricies egestas tellus, at posuere neque fermentum non. Cras in molestie neque. Donec ac vehicula mi, non commodo velit. Nam blandit eleifend lectus a vestibulum. Phasellus ut tortor et dui adipiscing vehicula. Maecenas lectus elit, sollicitudin quis ligula non, tempus consequat erat. Quisque laoreet urna eu nisi scelerisque, eget pulvinar mi fringilla. Donec nec dignissim augue.</p>
+            </div>
+        </div>
+	</div>
+    
+</div>
+
+</body>
+</html>
