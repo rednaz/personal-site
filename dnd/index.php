6b6e69827133b39e69dab53cfc5828db0416ab19
Eric 'Zander' Nelson <ci.eric.nelson@gmail.com>
2013-11-25 14:29:25 -0800

    reorganized front page

--- dnd/index.php
+++ dnd/index.php
@@ -38,17 +38,7 @@
             </div>
         </div>
 
-        <div class="panel1 panel-temp">
-            <div>
-                <h2>Updates</h2>
-                <ul>
-                    <li>Site Released!
-                        <p></p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="panel2">
+        <div class="panel2 panel-dbl">
                 <h2>Newest Intel</h2>
                 <a class="panel1" href="/dnd/factions/bardegi.php">
                     <div>
@@ -62,6 +52,28 @@
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, ipsum sed viverra auctor, dolor est varius magna, quis sodales lectus felis eu arcu. Nulla non luctus libero, venenatis vestibulum purus. Nunc erat augue, sagittis vitae odio eget, feugiat commodo mi. Donec nisl lacus, tincidunt in erat ac, luctus ultricies nibh. Mauris odio sapien, porta ac molestie vel, mattis eget leo. Phasellus ultricies egestas tellus, at posuere neque fermentum non. Cras in molestie neque. Donec ac vehicula mi, non commodo velit. Nam blandit eleifend lectus a vestibulum. Phasellus ut tortor et dui adipiscing vehicula. Maecenas lectus elit, sollicitudin quis ligula non, tempus consequat erat. Quisque laoreet urna eu nisi scelerisque, eget pulvinar mi fringilla. Donec nec dignissim augue.</p>
                     </div>
                 </a>
+                <a class="panel1" href="/dnd/characters/lochrauna.php">
+                    <div>
+                        <h3>Lochrauna</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, ipsum sed viverra auctor, dolor est varius magna, quis sodales lectus felis eu arcu. Nulla non luctus libero, venenatis vestibulum purus. Nunc erat augue, sagittis vitae odio eget, feugiat commodo mi. Donec nisl lacus, tincidunt in erat ac, luctus ultricies nibh. Mauris odio sapien, porta ac molestie vel, mattis eget leo. Phasellus ultricies egestas tellus, at posuere neque fermentum non. Cras in molestie neque. Donec ac vehicula mi, non commodo velit. Nam blandit eleifend lectus a vestibulum. Phasellus ut tortor et dui adipiscing vehicula. Maecenas lectus elit, sollicitudin quis ligula non, tempus consequat erat. Quisque laoreet urna eu nisi scelerisque, eget pulvinar mi fringilla. Donec nec dignissim augue.</p>
+                    </div>
+                </a>
+                <a class="panel1" href="/dnd/characters/meowly.php">
+                    <div>
+                        <h3>Meowly Cyrus</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, ipsum sed viverra auctor, dolor est varius magna, quis sodales lectus felis eu arcu. Nulla non luctus libero, venenatis vestibulum purus. Nunc erat augue, sagittis vitae odio eget, feugiat commodo mi. Donec nisl lacus, tincidunt in erat ac, luctus ultricies nibh. Mauris odio sapien, porta ac molestie vel, mattis eget leo. Phasellus ultricies egestas tellus, at posuere neque fermentum non. Cras in molestie neque. Donec ac vehicula mi, non commodo velit. Nam blandit eleifend lectus a vestibulum. Phasellus ut tortor et dui adipiscing vehicula. Maecenas lectus elit, sollicitudin quis ligula non, tempus consequat erat. Quisque laoreet urna eu nisi scelerisque, eget pulvinar mi fringilla. Donec nec dignissim augue.</p>
+                    </div>
+                </a>
+        </div>
+        <div class="panel1 panel-temp">
+            <div>
+                <h2>Updates</h2>
+                <ul>
+                    <li>Site Released!
+                        <p></p>
+                    </li>
+                </ul>
+            </div>
         </div>
         <div class="panel1 panel-temp">
             <div>
@@ -81,21 +93,6 @@
                     </li>
                 </ul>
             </div>
-        </div>
-        <div class="panel2">
-                <h2>Newest Party Members</h2>
-                <a class="panel1" href="/dnd/characters/lochrauna.php">
-                    <div>
-                        <h3>Lochrauna</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, ipsum sed viverra auctor, dolor est varius magna, quis sodales lectus felis eu arcu. Nulla non luctus libero, venenatis vestibulum purus. Nunc erat augue, sagittis vitae odio eget, feugiat commodo mi. Donec nisl lacus, tincidunt in erat ac, luctus ultricies nibh. Mauris odio sapien, porta ac molestie vel, mattis eget leo. Phasellus ultricies egestas tellus, at posuere neque fermentum non. Cras in molestie neque. Donec ac vehicula mi, non commodo velit. Nam blandit eleifend lectus a vestibulum. Phasellus ut tortor et dui adipiscing vehicula. Maecenas lectus elit, sollicitudin quis ligula non, tempus consequat erat. Quisque laoreet urna eu nisi scelerisque, eget pulvinar mi fringilla. Donec nec dignissim augue.</p>
-                    </div>
-                </a>
-                <a class="panel1" href="/dnd/characters/meowly.php">
-                    <div>
-                        <h3>Meowly Cyrus</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, ipsum sed viverra auctor, dolor est varius magna, quis sodales lectus felis eu arcu. Nulla non luctus libero, venenatis vestibulum purus. Nunc erat augue, sagittis vitae odio eget, feugiat commodo mi. Donec nisl lacus, tincidunt in erat ac, luctus ultricies nibh. Mauris odio sapien, porta ac molestie vel, mattis eget leo. Phasellus ultricies egestas tellus, at posuere neque fermentum non. Cras in molestie neque. Donec ac vehicula mi, non commodo velit. Nam blandit eleifend lectus a vestibulum. Phasellus ut tortor et dui adipiscing vehicula. Maecenas lectus elit, sollicitudin quis ligula non, tempus consequat erat. Quisque laoreet urna eu nisi scelerisque, eget pulvinar mi fringilla. Donec nec dignissim augue.</p>
-                    </div>
-                </a>
         </div>
 	</div>
     
