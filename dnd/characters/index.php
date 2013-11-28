28684ec3bdf8ef8e8128c14450c6dd8119c97794
rednaz <zander.nelson@gmail.com>
2013-11-27 23:02:03 -0800

    updated char index content

--- dnd/characters/index.php
+++ dnd/characters/index.php
@@ -29,11 +29,11 @@
         <h1>Characters</h1>
 
         <div class="panel3 panel-auto">
-
+            <div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, ipsum sed viverra auctor, dolor est varius magna, quis sodales lectus felis eu arcu. Nulla non luctus libero, venenatis vestibulum purus. Nunc erat augue, sagittis vitae odio eget, feugiat commodo mi. Donec nisl lacus, tincidunt in erat ac, luctus ultricies nibh. Mauris odio sapien, porta ac molestie vel, mattis eget leo. Phasellus ultricies egestas tellus, at posuere neque fermentum non. Cras in molestie neque. Donec ac vehicula mi, non commodo velit. Nam blandit eleifend lectus a vestibulum. Phasellus ut tortor et dui adipiscing vehicula. Maecenas lectus elit, sollicitudin quis ligula non, tempus consequat erat. Quisque laoreet urna eu nisi scelerisque, eget pulvinar mi fringilla. Donec nec dignissim augue.
                 
                 Morbi egestas vehicula ante sed congue. Maecenas at enim quis odio varius accumsan. Proin quis mauris fermentum, tempor odio et, blandit neque. Etiam pretium turpis non metus gravida, ac cursus massa ultricies. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin ac arcu congue, viverra justo euismod, suscipit augue. Suspendisse ornare, nulla eget placerat facilisis, leo lorem pulvinar eros, eget pulvinar orci erat eu sem. Aliquam bibendum tortor sit amet ante porta mollis. Donec vestibulum dui et ipsum sodales dictum. In sagittis nunc id mi sagittis convallis. Praesent vel feugiat erat. Nulla faucibus, nisl at feugiat accumsan, magna tortor tempor augue, quis mollis lorem urna eu felis. Pellentesque lacinia, nisl a laoreet euismod, odio massa mattis enim, in viverra ante mauris at velit. Ut mattis non velit eu ultricies. Donec iaculis lorem non risus viverra, quis rhoncus tellus gravida.</p>
-   
+            </div>
         </div>
 
         <div class="panel3 panel-auto">
@@ -48,19 +48,19 @@
                 <a class="panel1" href="../characters/bremusa.php">
                     <div>
                         <h3>Bremusa of Stella</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, ipsum sed viverra auctor, dolor est varius magna, quis sodales lectus felis eu arcu. Nulla non luctus libero, venenatis vestibulum purus. Nunc erat augue, sagittis vitae odio eget, feugiat commodo mi. Donec nisl lacus, tincidunt in erat ac, luctus ultricies nibh. Mauris odio sapien, porta ac molestie vel, mattis eget leo. Phasellus ultricies egestas tellus, at posuere neque fermentum non. Cras in molestie neque. Donec ac vehicula mi, non commodo velit. Nam blandit eleifend lectus a vestibulum. Phasellus ut tortor et dui adipiscing vehicula. Maecenas lectus elit, sollicitudin quis ligula non, tempus consequat erat. Quisque laoreet urna eu nisi scelerisque, eget pulvinar mi fringilla. Donec nec dignissim augue.</p>
+                        <p>Setareh, the land of stars.  Legends state that the mountain top used to be so high that you could reach other worlds.  The mountain range itself was even in the shape of a star.  While many different villages and towns were founded all along the ranges themselves, none were built along the center: an active volcano.  Some ventured to the volcano for the thrill or historical purposes, but only one group actually decided to live around the volcano.  Considered both brave and stupid for doing so, this group was aptly referred to as the Barbarians of Setareh.</p>
                     </div>
                 </a>
                 <a class="panel1" href="../characters/dotty.php">
                     <div>
                         <h3>Dotty Snow</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, ipsum sed viverra auctor, dolor est varius magna, quis sodales lectus felis eu arcu. Nulla non luctus libero, venenatis vestibulum purus. Nunc erat augue, sagittis vitae odio eget, feugiat commodo mi. Donec nisl lacus, tincidunt in erat ac, luctus ultricies nibh. Mauris odio sapien, porta ac molestie vel, mattis eget leo. Phasellus ultricies egestas tellus, at posuere neque fermentum non. Cras in molestie neque. Donec ac vehicula mi, non commodo velit. Nam blandit eleifend lectus a vestibulum. Phasellus ut tortor et dui adipiscing vehicula. Maecenas lectus elit, sollicitudin quis ligula non, tempus consequat erat. Quisque laoreet urna eu nisi scelerisque, eget pulvinar mi fringilla. Donec nec dignissim augue.</p>
+                        <p>Growing up  was never easy for Dotty.  This was  mostly his own fault though.  The only  thing that was actually rough for him was growing up without a father.  Being a bastard was probably the reason why  he decided to be a smartass though.  Like  most boys in similar situations, he would act out due to the fact that he  didn&rsquo;t have a father figure.  Unlike most  boys though, he mostly stuck to harmless mischief.  After all, he was only looking for attention,  not hatred.</p>
                     </div>
                 </a>
                 <a class="panel1" href="../characters/catriona.php">
                     <div>
                         <h3>Catr&iacute;ona</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, ipsum sed viverra auctor, dolor est varius magna, quis sodales lectus felis eu arcu. Nulla non luctus libero, venenatis vestibulum purus. Nunc erat augue, sagittis vitae odio eget, feugiat commodo mi. Donec nisl lacus, tincidunt in erat ac, luctus ultricies nibh. Mauris odio sapien, porta ac molestie vel, mattis eget leo. Phasellus ultricies egestas tellus, at posuere neque fermentum non. Cras in molestie neque. Donec ac vehicula mi, non commodo velit. Nam blandit eleifend lectus a vestibulum. Phasellus ut tortor et dui adipiscing vehicula. Maecenas lectus elit, sollicitudin quis ligula non, tempus consequat erat. Quisque laoreet urna eu nisi scelerisque, eget pulvinar mi fringilla. Donec nec dignissim augue.</p>
+                        <p>Catr&iacute;ona  grew up with three brothers: one was ten years her elder, the second was eight  years her elder, and the youngest was five years younger than she. As a  consequence of this, she tended to be more of a tomboy in her attitude, yet as  the only daughter of the family she was also pampered as the little princess  which also shows through her attitude occasionally.</p>
                     </div>
                 </a>
                 <a class="panel1" href="../characters/miziki.php">
@@ -91,7 +91,7 @@
             <a class="panel1" href="../characters/pandamore.php">
                 <div>
                     <h3>Pandamore</h3>
-                    <p>An elven dancer who had the strange inclination to disguise himself has goblins by wearing their skin. Killed in battle by a Very Young Blue Dragon.</p>
+                    <p>Pandamore was making a name for himself.  He was a pretty good dancer, and was entertaining patrons all across Bashkoh.  He was even getting requests from rich patrons.  The only problem was that the profession wasn&rsquo;t really providing him with the income that he had desired.  Luckily for Pandamore, that was the plan.</p>
                 </div>
             </a>
         </div>
