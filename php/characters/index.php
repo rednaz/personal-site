c004f71ffdb79c294da1fc540fbd14a463248c31
Eric 'Zander' Nelson <ci.eric.nelson@gmail.com>
2014-01-15 14:57:09 -0800

    ie9 document fix and start of ie8 fixes

--- php/characters/index.php
+++ php/characters/index.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
 <!--
 
     charcters home
@@ -9,10 +12,6 @@
     zandernelsoninc.com
 
                                         -->
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
 <meta http-equiv="content-cell-Type" content-cell="text/html; charset=utf-8" />
 <link type="text/css" rel="stylesheet" href="../../css/sass.css" />
 <title>Characters</title>
